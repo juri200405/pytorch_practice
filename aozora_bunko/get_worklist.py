987bc35717f8f92ce529dc129b43333ed3038fc3
juri200405 <juri200405@gmail.com>
2019-06-20 21:36:10 +0900

    青空文庫の作品一覧の取得成功

--- aozora_bunko/get_worklist.py
+++ aozora_bunko/get_worklist.py
@@ -1,12 +1,7 @@
 import zipfile
 import urllib.request
+from pathlib import Path
 
 if __name__ == "__main__":
-    #req = urllib.request.Request("https://www.aozora.gr.jp/index_pages/person_all.html")
-    req = urllib.request.Request("https://www.aozora.gr.jp/")
-    req.set_proxy("proxy.uec.ac.jp:8080", 'https')
-    with urllib.request.urlopen(req) as response:
-        file_data = response.read()
-        print(file_data.decode("utf8"))
-        #with open("list_person_all_extended_utf8.zip", 'wb') as f:
-            #f.write(file_data)
+    data_path = Path("./aozora_bunko/datas")
+    urllib.request.urlretrieve("https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip", str(data_path / "list_person_all_extended_utf8.zip"))
