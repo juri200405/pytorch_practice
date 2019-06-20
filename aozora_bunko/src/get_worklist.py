81b39df7fdc2acb197b985c498d3a87b7f21cf1b
juri200405 <juri200405@gmail.com>
2019-06-20 21:45:54 +0900

    zipファイルの解凍及び関数構成の変更

--- aozora_bunko/src/get_worklist.py
+++ aozora_bunko/src/get_worklist.py
@@ -2,6 +2,14 @@ import zipfile
 import urllib.request
 from pathlib import Path
 
+def get_zip_file(dpath):
+    urllib.request.urlretrieve("https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip", str(data_path / "list_person_all_extended_utf8.zip"))
+
+def unzip(dpath):
+    with zipfile.ZipFile(str(data_path / "list_person_all_extended_utf8.zip")) as existing_zip:
+        existing_zip.extractall(str(data_path))
+
 if __name__ == "__main__":
     data_path = Path("./aozora_bunko/datas")
-    urllib.request.urlretrieve("https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip", str(data_path / "list_person_all_extended_utf8.zip"))
+    #get_zip_file(data_path)
+    #unzip(data_path)
