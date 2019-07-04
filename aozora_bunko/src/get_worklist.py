f4b50687c211d97639b77656055ee29f0885edd5
juri200405 <juri200405@gmail.com>
2019-07-04 15:26:53 +0900

    出力ファイルを変更
    ある程度のところでファイルを切り替えるようにした。
    ファイルを開く文字コードの変更

--- aozora_bunko/src/get_worklist.py
+++ aozora_bunko/src/get_worklist.py
@@ -14,7 +14,7 @@ def unzip(dpath):
 
 def get_worklist(dpath):
     worklist = []
-    with open(str(dpath / "list_person_all_extended_utf8.csv"), newline='', encoding='utf8') as f:
+    with open(str(dpath / "list_person_all_extended_utf8.csv"), newline='', encoding='utf_8') as f:
         reader = csv.DictReader(f)
         for row in reader:
             if(row["分類番号"] == "NDC 913" and row["文字遣い種別"] == "新字新仮名" and row["作品著作権フラグ"] == "なし" and not row["テキストファイルURL"] == ""):
@@ -36,54 +36,61 @@ def extract_text_zip(zip_path, zip_name, text_path):
     return file_name
 
 def get_work(dpath, worklist, download=True):
-    i = 0
     zip_path = dpath / "zips"
     text_path = dpath / "texts"
 
-    with open(str(dpath / "text.txt"), 'wt', encoding='utf8') as fout:
-        for item in tqdm.tqdm(worklist[:10]):
-            zip_name = download_text_zip(item, zip_path, download)
-            file_name = extract_text_zip(zip_path, zip_name, text_path)
+    write_byte_num = 0
+    output_file_num = 0
+    fout = open(str(dpath / 'text_{:04d}_.txt'.format(output_file_num)), 'wt', encoding='utf_8')
+
+    for item in tqdm.tqdm(worklist):
+        zip_name = download_text_zip(item, zip_path, download)
+        file_name = extract_text_zip(zip_path, zip_name, text_path)
             
-            p_tmp = (text_path / file_name).glob('*.txt')
-            for text_file in p_tmp:
-                with open(str(text_file), 'rt', encoding='shiftjis') as fstream:
-                    fout.write(file_name)
-                    fout.write('\n')
-                    texts = fstream.read(-1)
+        p_tmp = (text_path / file_name).glob('*.txt')
+        for text_file in p_tmp:
+            with open(str(text_file), 'rt', encoding='shift_jisx0213') as fstream:
+                write_byte_num += fout.write(file_name)
+                write_byte_num += fout.write('\n')
+                texts = fstream.read(-1)
                     
-                    #作品名と著者名、及び注を削除
-                    texts = re.sub(r"^.+-+\s", "", texts, flags=re.DOTALL)
-                    texts = re.sub(r"^\s*", "", texts)
+                #作品名と著者名、及び注を削除
+                texts = re.sub(r"^.+-+\s", "", texts, flags=re.DOTALL)
+                texts = re.sub(r"^\s*", "", texts)
                     
-                    #文末の書誌情報の削除
-                    texts = re.sub(r"\s底本：.*$", "", texts, flags=re.DOTALL)
-
-                    #行頭のスペース、改行の削除
-                    texts = re.sub(r"^\s+", "", texts, flags=re.MULTILINE)
+                #文末の書誌情報の削除
+                texts = re.sub(r"\s底本：.*$", "", texts, flags=re.DOTALL)
                     
-                    #文中のルビを削除
-                    texts = re.sub(r"《.*?》", "", texts)
+                #行頭のスペース、改行の削除
+                texts = re.sub(r"^\s+", "", texts, flags=re.MULTILINE)
+                    
+                #文中のルビを削除
+                texts = re.sub(r"《.*?》", "", texts)
 
-                    #文中の注を削除
-                    texts = re.sub(r"［＃.*?］", "", texts)
+                #文中の注を削除
+                texts = re.sub(r"［＃.*?］", "", texts)
 
-                    #文中のルビ用の記号の削除
-                    texts = re.sub(r"｜", "", texts)
+                #文中のルビ用の記号の削除
+                texts = re.sub(r"｜", "", texts)
 
-                    #かぎかっこを別の行に
-                    texts = re.sub(r"(?<!\n)「", "\n「", texts)
-                    texts = re.sub(r"」(?!\n)", "」\n", texts)
+                #かぎかっこを別の行に
+                texts = re.sub(r"(?<!\n)「", "\n「", texts)
+                texts = re.sub(r"」(?!\n)", "」\n", texts)
 
-                    fout.write(texts)
-                    fout.write("\n\n\n")
-            i += 1
+                write_byte_num += fout.write(texts)
+                write_byte_num += fout.write("\n\n\n")
+        if write_byte_num > 1000000:
+            fout.close()
+            output_file_num += 1
+            write_byte_num = 0
+            fout = open(str(dpath / 'text_{:04d}_.txt'.format(output_file_num)), 'wt', encoding='utf_8')
+    fout.close()
 
 if __name__ == "__main__":
     use_proxy = True
     #use_prixy = False
-    download = True
-    #download = False
+    #download = True
+    download = False
     data_path = Path("./aozora_bunko/datas")
     if download:
         if use_proxy:
